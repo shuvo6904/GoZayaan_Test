38be3a308a8ffb437584553a7ddf4e27f8c57791
shuvo <shuvo6904@gmail.com>
2024-11-15 19:29:57 +0600

    feat(ui): Complete home page design with fixed top layout and scrollable content

--- app/src/main/java/com/shuvo6904/gozayaan/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/shuvo6904/gozayaan/presentation/home/HomeActivity.kt
@@ -1,10 +1,8 @@
-package com.shuvo6904.gozayaan
+package com.shuvo6904.gozayaan.presentation.home
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.shuvo6904.gozayaan.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
